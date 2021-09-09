dbe25def9d571e622b29e06bfd8be924d89d80b9
Gyash777 <90402402+Gyash777@users.noreply.github.com>
2021-09-09 20:12:58 +0530

    Add files via upload

---
+++ sizes of datatypes.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int intType;
+	char charType;
+	float floatType;
+	double doubleType;
+	
+	cout<<" Size of int is : "<<sizeof(intType)<<"\n";
+	cout<<" Size of char is : "<<sizeof(charType)<<"\n";
+	cout<<" Size of float is : "<<sizeof(floatType)<<"\n";
+	cout<<" Size of double is : "<<sizeof(doubleType)<<"\n";
+	
+	return 0;
+	
+}
