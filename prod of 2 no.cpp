21dc9f07ab3450cc0b7eec267e4de3e06988f0e7
Gyash777 <90402402+Gyash777@users.noreply.github.com>
2021-09-09 20:13:34 +0530

    Add files via upload

---
+++ prod of 2 no.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	float n1,n2,res;
+	cout<<"Enter two no.'s : \n";
+	cin>>n1>>n2;
+	res=n1*n2;
+	cout<<" Product of"<<n1<<"and"<<n2<<"is"<<res;
+	return 0;
+	
+}
+
